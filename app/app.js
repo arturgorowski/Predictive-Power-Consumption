3028482a09fa7eb0d08a63cbd91aae5094296476
arturgorowski <artek159@gmail.com>
2019-07-01 15:33:29 +0200

    something

--- app/app.js
+++ app/app.js
@@ -14,80 +14,81 @@ let energyClass = '',
     annualEnergyConsumption = '',
     powerType;
 
-// tvList.forEach((url, k) => {
-//     console.log(url[1])
-//     limiter.request(url[1], function (error, response, html) {
-//         if (!error && response.statusCode == 200) {
-//             try {
-                
-//                 const $ = cheerio.load(html)
+tvList.forEach((url, k) => {
+    const firstUrl = url[0]
 
-//                 let div = $('div.m-offerShowData')
-//                 const powerNode = div[0].childNodes[13];
+    console.log(firstUrl)
+    request(firstUrl, function (error, response, html) {
+        if (!error && response.statusCode == 200) {
+            try {
+                const $ = cheerio.load(html)
 
-//                 powerNode.children.forEach((item, k) => {
-//                     if (item.type !== "text") {
+                let div = $('div.m-offerShowData')
+                const powerNode = div[0].childNodes[13];
 
-//                         if (k === 3) energyClass = $(item).find("dd").text().trim()
-//                         if (k === 5) powerConsumption = Number($(item).find("dd").text().trim())
-//                         if (k === 7) powerConsumptionStandby = Number($(item).find("dd").text().trim())
-//                         if (k === 9) annualEnergyConsumption = Number($(item).find("dd").text().trim());
-//                         if (k === 11) powerType = $(item).find("dd").text().trim();
+                powerNode.children.forEach((item, k) => {
+                    if (item.type !== "text") {
 
-//                         let dt = $(item).find("dt").text().trim()
-//                         let dd = $(item).find("dd").text().trim()
-//                     }
-//                 })
+                        if (k === 3) energyClass = $(item).find("dd").text().trim()
+                        if (k === 5) powerConsumption = Number($(item).find("dd").text().trim())
+                        if (k === 7) powerConsumptionStandby = Number($(item).find("dd").text().trim())
+                        if (k === 9) annualEnergyConsumption = Number($(item).find("dd").text().trim());
+                        if (k === 11) powerType = $(item).find("dd").text().trim();
 
-//                 data.push({
-//                     energyClass,
-//                     powerConsumption,
-//                     powerConsumptionStandby,
-//                     annualEnergyConsumption,
-//                     powerType
-//                 })
-//             } catch (error) {
-//                 throw error
-//             }
-//         }
-//     })
-//     console.log(data)
-// })
+                        let dt = $(item).find("dt").text().trim()
+                        let dd = $(item).find("dd").text().trim()
+                    }
+                })
 
-limiter.request(mediaMarktTv, function (error, response, html) {
-    if (!error && response.statusCode == 200) {
-        try {
-            const $ = cheerio.load(html)
-
-            let div = $('div.m-offerShowData')
-            const powerNode = div[0].childNodes[13];
-
-            powerNode.children.forEach((item, k) => {
-                if (item.type !== "text") {
-
-                    if (k === 3) energyClass = $(item).find("dd").text().trim()
-                    if (k === 5) powerConsumption = Number($(item).find("dd").text().trim())
-                    if (k === 7) powerConsumptionStandby = Number($(item).find("dd").text().trim())
-                    if (k === 9) annualEnergyConsumption = Number($(item).find("dd").text().trim());
-                    if (k === 11) powerType = $(item).find("dd").text().trim();
-
-                    let dt = $(item).find("dt").text().trim()
-                    let dd = $(item).find("dd").text().trim()
-                }
-            })
-
-            data.push({
-                energyClass,
-                powerConsumption,
-                powerConsumptionStandby,
-                annualEnergyConsumption,
-                powerType
-            })
-
-            console.log(data)
-
-        } catch (error) {
-            throw error
+                data.push({
+                    energyClass,
+                    powerConsumption,
+                    powerConsumptionStandby,
+                    annualEnergyConsumption,
+                    powerType
+                })
+            } catch (error) {
+                throw error
+            }
         }
-    }
-});
+    })
+    console.log(data)
+})
+
+// limiter.request(mediaMarktTv, function (error, response, html) {
+//     if (!error && response.statusCode == 200) {
+//         try {
+//             const $ = cheerio.load(html)
+
+//             let div = $('div.m-offerShowData')
+//             const powerNode = div[0].childNodes[13];
+
+//             powerNode.children.forEach((item, k) => {
+//                 if (item.type !== "text") {
+
+//                     if (k === 3) energyClass = $(item).find("dd").text().trim()
+//                     if (k === 5) powerConsumption = Number($(item).find("dd").text().trim())
+//                     if (k === 7) powerConsumptionStandby = Number($(item).find("dd").text().trim())
+//                     if (k === 9) annualEnergyConsumption = Number($(item).find("dd").text().trim());
+//                     if (k === 11) powerType = $(item).find("dd").text().trim();
+
+//                     let dt = $(item).find("dt").text().trim()
+//                     let dd = $(item).find("dd").text().trim()
+//                 }
+//             })
+
+//             data.push({
+//                 energyClass,
+//                 powerConsumption,
+//                 powerConsumptionStandby,
+//                 annualEnergyConsumption,
+//                 powerType
+//             })
+
+//             console.log(data)
+
+//         } catch (error) {
+//             throw error
+//         }
+//     }
+// });
