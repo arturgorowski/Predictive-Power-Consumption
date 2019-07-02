390733d15cebeb9877a54b2981465de5ae00e6f7
arturgorowski <artek159@gmail.com>
2019-07-02 15:59:11 +0200

    day two

--- app/app.js
+++ app/app.js
@@ -1,7 +1,10 @@
-var tvList = require('./temp');
-var request = require('request');
+//var tvListUrl = require('./temp');
+var tvListUrl = require('./temp2');
 var cheerio = require('cheerio');
 var RateLimiter = require('request-rate-limiter');
+const fetch = require('node-fetch');
+const axios = require('axios');
+const request = require('request');
 
 var limiter = new RateLimiter(120);
 const mediaMarktUrl = 'https://mediamarkt.pl/rtv-i-telewizory/telewizory'
@@ -14,81 +17,105 @@ let energyClass = '',
     annualEnergyConsumption = '',
     powerType;
 
-tvList.forEach((url, k) => {
-    const firstUrl = url[0]
+function parseResponseHtml(html, i) {
+    try {
+        const $ = cheerio.load(html[i])
+        console.log(html)
+        let div = $('div.m-offerShowData')
+        //console.log("div >>>", div)
+        const powerNode = div[0].childNodes[13];
+        //console.log("powerNode >>>", powerNode)
 
-    console.log(firstUrl)
-    request(firstUrl, function (error, response, html) {
-        if (!error && response.statusCode == 200) {
-            try {
-                const $ = cheerio.load(html)
+        powerNode.children.forEach((item, k) => {
+            if (item.type !== "text") {
 
-                let div = $('div.m-offerShowData')
-                const powerNode = div[0].childNodes[13];
-
-                powerNode.children.forEach((item, k) => {
-                    if (item.type !== "text") {
-
-                        if (k === 3) energyClass = $(item).find("dd").text().trim()
-                        if (k === 5) powerConsumption = Number($(item).find("dd").text().trim())
-                        if (k === 7) powerConsumptionStandby = Number($(item).find("dd").text().trim())
-                        if (k === 9) annualEnergyConsumption = Number($(item).find("dd").text().trim());
-                        if (k === 11) powerType = $(item).find("dd").text().trim();
-
-                        let dt = $(item).find("dt").text().trim()
-                        let dd = $(item).find("dd").text().trim()
-                    }
-                })
-
-                data.push({
-                    energyClass,
-                    powerConsumption,
-                    powerConsumptionStandby,
-                    annualEnergyConsumption,
-                    powerType
-                })
-            } catch (error) {
-                throw error
+                if (k === 3) energyClass = $(item).find("dd").text().trim()
+                if (k === 5) powerConsumption = Number($(item).find("dd").text().trim())
+                if (k === 7) powerConsumptionStandby = Number($(item).find("dd").text().trim())
+                if (k === 9) annualEnergyConsumption = Number($(item).find("dd").text().trim());
+                if (k === 11) powerType = $(item).find("dd").text().trim();
             }
-        }
-    })
-    console.log(data)
+        })
+        
+        data.push({
+            energyClass,
+            powerConsumption,
+            powerConsumptionStandby,
+            annualEnergyConsumption,
+            powerType
+        })
+    } catch (error) {
+        throw error
+    }
+    console.log(">>> data", data)
+}
+
+let promises = tvListUrl[0].map(url => {
+    console.log(">>> url", url)
+    //for (let i = 0; i < url.length; i++) {
+        //console.log(url[i])
+        return new Promise((resolve, reject) => {
+            request(url, function (error, response, html) {
+                if (!error && response.statusCode == 200) {
+                    //console.log(">>> body", html)    
+                    resolve(html);
+                } else {
+                    reject(error);
+                }
+            });
+        });
+    //}
+
 })
+console.log(promises)
+let iterator = 4
+promises.reduce((promiseChain, currentTask) => {
+    return promiseChain
+        .then(chainResults => currentTask
+            .then(currentResult => chainResults = chainResults
+                .concat(currentResult)
+            )
+        );
+}, Promise.resolve(promises))
+    .then(htmlBody => {
+        //console.log(">>> htmlBody", htmlBody[1]);
+        iterator +=1
+        parseResponseHtml(htmlBody, iterator)
+        //return htmlBody;
+    }).then(model => {
+        //console.log(">>> model ", model);
+        return model;
+    }).catch(err => {
+        console.error(">>> ERR :: ", err);
+        return err;
+    });
 
-// limiter.request(mediaMarktTv, function (error, response, html) {
-//     if (!error && response.statusCode == 200) {
-//         try {
-//             const $ = cheerio.load(html)
+// Promise.all(promises)
+//     .then(html => {
+//         console.log(html)
+//         const $ = cheerio.load(html)
+//         let div = $('div.m-offerShowData')
+//         const powerNode = div[0].childNodes[13];
 
-//             let div = $('div.m-offerShowData')
-//             const powerNode = div[0].childNodes[13];
+//         powerNode.children.forEach((item, k) => {
+//             if (item.type !== "text") {
 
-//             powerNode.children.forEach((item, k) => {
-//                 if (item.type !== "text") {
-
-//                     if (k === 3) energyClass = $(item).find("dd").text().trim()
-//                     if (k === 5) powerConsumption = Number($(item).find("dd").text().trim())
-//                     if (k === 7) powerConsumptionStandby = Number($(item).find("dd").text().trim())
-//                     if (k === 9) annualEnergyConsumption = Number($(item).find("dd").text().trim());
-//                     if (k === 11) powerType = $(item).find("dd").text().trim();
-
-//                     let dt = $(item).find("dt").text().trim()
-//                     let dd = $(item).find("dd").text().trim()
-//                 }
-//             })
-
-//             data.push({
-//                 energyClass,
-//                 powerConsumption,
-//                 powerConsumptionStandby,
-//                 annualEnergyConsumption,
-//                 powerType
-//             })
-
-//             console.log(data)
-
-//         } catch (error) {
-//             throw error
-//         }
-//     }
-// });
+//                 if (k === 3) energyClass = $(item).find("dd").text().trim()
+//                 if (k === 5) powerConsumption = Number($(item).find("dd").text().trim())
+//                 if (k === 7) powerConsumptionStandby = Number($(item).find("dd").text().trim())
+//                 if (k === 9) annualEnergyConsumption = Number($(item).find("dd").text().trim());
+//                 if (k === 11) powerType = $(item).find("dd").text().trim();
+//             }
+//         })
+//         data.push({
+//             energyClass,
+//             powerConsumption,
+//             powerConsumptionStandby,
+//             annualEnergyConsumption,
+//             powerType
+//         })
+//         console.log(data)
+//     })
+//     .catch(error => {
+//         throw error
+//     })
---
+++ app/temp2.js
@@ -0,0 +1,10 @@
+const tvListUrl = [
+    "https://mediamarkt.pl/rtv-i-telewizory/telewizor-qled-samsung-qe55q60rat",
+    "https://mediamarkt.pl/rtv-i-telewizory/telewizor-qled-samsung-qe55q60rat",
+    "https://mediamarkt.pl/rtv-i-telewizory/telewizor-qled-samsung-qe65q90rat",
+    "https://mediamarkt.pl/rtv-i-telewizory/telewizor-qled-samsung-qe65q90rat",
+    "https://mediamarkt.pl/rtv-i-telewizory/telewizor-philips-55pus6703-12"
+]
+
+
+module.exports = [tvListUrl]  
