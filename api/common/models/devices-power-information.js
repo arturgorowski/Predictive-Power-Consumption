59adea2522615355a718b449e7551b9f3d75f13b
arturgorowski <artek159@gmail.com>
2019-08-02 16:07:38 +0200

    text search

--- api/common/models/devices-power-information.js
+++ api/common/models/devices-power-information.js
@@ -1,20 +1,54 @@
 'use strict';
 
-module.exports = function(Devicespowerinformation) {
+module.exports = function (Devicespowerinformation) {
 
-    Devicespowerinformation.findAll = function(search, req, cb) {
+    // Devicespowerinformation.findAll = function(search, req, cb) {
 
-        let query = { where: {} };
+    //     let query = { where: {} };
+
+    //     if (req.query.per_page) query.limit = req.query.per_page;
+    //     if (req.query.per_page && req.query.page) query.skip = (req.query.per_page * (req.query.page - 1));
+    //     if (req.query.sort && req.query.order) query.order = req.query.sort + " " + req.query.order;
+
+    //     //{ "where": {"$text": { "search": "sony"} } }
+    //     if (search !== undefined) {
+    //         //search.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+    //         let regQuery = new RegExp(search, "i");
+    //         query.where = { "$text": { "search": search } };
+    //     }
+
+    //     try {
+    //         req.query.filter = JSON.parse(req.query.filter);
+    //         // merge filters
+    //         if (typeof req.query.filter === "object") {
+    //             query = merge(query, req.query.filter);
+    //         }
+
+    //     } catch (err) {}
+
+    //     Devicespowerinformation.count(query.where).then(function(count) {
+    //         return Devicespowerinformation.find(query).then(function(results) {
+
+    //             return cb(null, { total: count, result: results });
+
+    //         });
+    //     }).catch(function(err) {
+    //         return cb(err, null);
+    //     });
+    // };
+
+        Devicespowerinformation.findAll = function(search, req, cb) {
+
+        let query = { where: [] };
 
         if (req.query.per_page) query.limit = req.query.per_page;
         if (req.query.per_page && req.query.page) query.skip = (req.query.per_page * (req.query.page - 1));
         if (req.query.sort && req.query.order) query.order = req.query.sort + " " + req.query.order;
 
-        //{ "where": {"$text": { "search": "sony"} } }
         if (search !== undefined) {
             //search.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
             let regQuery = new RegExp(search, "i");
-            query.where = { "$text": { "search": search } };
+            query.where = {"$or" : [{ "producent":  regQuery }, { "model":  regQuery }, { "productName":  regQuery }]};
         }
 
         try {
@@ -26,7 +60,7 @@ module.exports = function(Devicespowerinformation) {
 
         } catch (err) {}
 
-        Devicespowerinformation.count(query.where).then(function(count) {
+        Devicespowerinformation.count(query.$or).then(function(count) {
             return Devicespowerinformation.find(query).then(function(results) {
 
                 return cb(null, { total: count, result: results });
@@ -36,6 +70,8 @@ module.exports = function(Devicespowerinformation) {
             return cb(err, null);
         });
     };
+
+
 
     // --------------------------------- REMOTE METHODS ----------------------------------
 
